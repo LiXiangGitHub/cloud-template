a6bf7fa95bf669ae6803b52f3a948a8a59e9403d
80795289@qq.com <qq123456>
2019-08-27 17:54:21 +0800

    报错bug修复

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
     chainWebpack: config => {
+        config.devServer.set('inline', false)
+        config.devServer.set('hot', false)
         config.externals(['vue', 'vue-router'])
     },
     filenameHashing: false,
     // 打包输入文件名称
     outputDir: process.env.VUE_APP_NAME,
+    productionSourceMap: false
 }
